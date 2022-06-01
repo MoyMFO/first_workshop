6a88293a6e2cdae6fa632c77b32ef7c4de93faa0
Moises Flores <floresmoisesortiz@gmail.com>
2022-05-31 20:08:19 -0500

    More metrics were added. Also, visualization progress was reached

--- functions.py
+++ functions.py
@@ -21,7 +21,6 @@ def f_descriptive_ob(data_ob:dict) -> dict:
     """
     Parameters 
     ----------
-
     data_ob: dict (default: None)
         Datos de entrada del libro de ordenes, es un diccionario con la siguiente estructura:
         'timestamp': objeto tipo timestamp reconocible por maquina, e.g. pd.to_datetime()
@@ -57,21 +56,51 @@ def f_descriptive_ob(data_ob:dict) -> dict:
     ob_m7 = [np.round(data_ob[i_ts]['bid_size'].sum() + data_ob[i_ts]['ask_size'].sum(), 6) for i_ts in ob_ts]
 
     # -- OrderBook Imbalance (v: volume, d: depth) -- #
-    obimb = lambda v, d: np.sum(v[0][:d])/np.sum([v[0][:d], v[1][:d]])
+    # v[0] Bid volume, v[1] Ask volume 
+    obimb = lambda v, d: np.sum(v.iloc[:d,0])/np.sum([v.iloc[:d,0], v.iloc[:d,1]])
+    ob_m8 = [obimb(data_ob[i_ts][['bid_size','ask_size']],2) for i_ts in ob_ts]
 
+    # -- wighted-Midprice (p: price, v: volume)
+    w_midprice = lambda p, v: (v.iloc[:,1]/np.sum([v.iloc[:,0], v.iloc[:,1]]))*p.iloc[:,0] + (v.iloc[:,0]/np.sum([v.iloc[:,0], v.iloc[:,1]]))*p.iloc[:,1]
+    ob_m9  = [w_midprice(data_ob[i_ts][['bid','ask']], data_ob[i_ts][['bid_size', 'ask_size']]) for i_ts in ob_ts]
 
-    r_data = {'median_ts_ob': ob_m1, 'spread': ob_m2, 'midprice': ob_m3}
+    # -- VWAP (Volume-Weighted Average Price) (p: price, v: volume, d:depth)
+    # p[0]:Bid price, p[1]:Ask price, v[0]: Bid volume, v[1]: Ask volume
+    vwap = lambda p, v, d: np.sum(p.iloc[:d, 0] * v.iloc[:d,0] + p.iloc[:d,1] * v.iloc[:d,1]) / np.sum(v.iloc[:d,0] + v.iloc[:d,1])
+    ob_m10 = [vwap(data_ob[i_ts][['bid', 'ask']], data_ob[i_ts][['bid_size', 'ask_size']], 10) for i_ts in ob_ts]
+
+    r_data = {'median_ts_ob': ob_m1, 'spread': ob_m2, 'midprice': ob_m3, "Orderbook Imbalance": ob_m8, 
+              'wighted-Midprice': ob_m9,'VWAP': ob_m10}
 
     return r_data
+
+
+# -- OrderBook Imbalance (v: volume, d: depth) -- #
+# v[0] Bid volume, v[1] Ask volume 
+#obimb = lambda v, d: np.sum(v.iloc[:d,0])/np.sum([v.iloc[:d,0], v.iloc[:d,1]])
+#ob_m8 = [obimb(data_ob[i_ts][['bid_size','ask_size']],2) for i_ts in ob_ts]
+
+# -- wighted-Midprice (p: price, v: volume)
+#w_midprice = lambda p, v: (v[1]/np.sum([v[0], v[1]]))*p[0] + (v[0]/np.sum([v[0], v[1]]))*p[1]
+#w_midprice = lambda p, v: (v.iloc[:,1]/np.sum([v.iloc[:,0], v.iloc[:,1]]))*p.iloc[:,0] + (v.iloc[:,0]/np.sum([v.iloc[:,0], v.iloc[:,1]]))*p.iloc[:,1]
+#ob_m9  = [w_midprice(data_ob[i_ts][['bid','ask']], data_ob[i_ts][['bid_size', 'ask_size']]) for i_ts in ob_ts]
+
+
+# -- VWAP (Volume-Weighted Average Price) (p: price, v: volume, d:depth)
+# p[0]:Bid price, p[1]:Ask price, v[0]: Bid volume, v[1]: Ask volume
+#vwap = lambda p, v, d: np.sum(p[0][:d] * v[0][:d] + p[1][:d] * v[1][:d]) / np.sum(v[0][:d] + v[1][:d])
+#vwap = lambda p, v, d: np.sum(p.iloc[:d, 0] * v.iloc[:d,0] + p.iloc[:d,1] * v.iloc[:d,1]) / np.sum(v.iloc[:d,0] + v.iloc[:d,1])
+#ob_10 = [vwap(data_ob[i_ts][['bid', 'ask']], data_ob[i_ts][['bid_size', 'ask_size']], 10) for i_ts in ob_ts]
+#ob_10
+
+
+
+
 """
 numero = []
 for i in range():
     numero.append(obimb(v=data_1[i], d=10))
 df_data['ob_imb'] = numero
 df_data['ob_imb'] = [obimb(v=data_1, d=10) for renglon in todo_dataframe]    
-"""
 
-# Time of orderbook update
-# Spread, Midprice
-# No. Price levels
-# Bid_Volume, Ask_Volume, Total-Volume
+"""
--- main.py
+++ main.py
@@ -11,6 +11,8 @@
 
 import data as dt
 import functions as fn
+import numpy as np
+import visualizations as vz
 
 # Read input data
 data_ob = dt.ob_data
@@ -20,10 +22,27 @@ n_libros = len(list(data_ob.keys()))
 print(f"La cantidad total de libros de ordenes es: {n_libros}")
 
 # OrderBook Metrics
-data_1 = fn.f_descriptive_ob(data_ob=data_ob)
+data_1 = fn.f_descriptive_ob(data_ob=data_ob) 
 
 # Celda 3
 print(f"Mediana del tiempo esperado para un nuevo OrderBook: {data_1['median_ts_ob']}")
 
 # Celda 4
-print(f"Mediana del tiempo esperado para un nuevo OrderBook: {data_1['median_ts_ob']}")
+print(f"Mediana del tiempo esperado para un nuevo OrderBook: {data_1['Orderbook Imbalance']}")
+
+# -- Ejercicios de repaso
+data_ob[list(data_ob.keys())[1]]['bid_size'][0]
+
+libro_0 = data_ob[list(data_ob.keys())[0]].copy()
+
+libro_0.index = libro_0.index.astype(np.int64)
+
+# Compresion de listas [elemento1, elemento2, elemento3...]
+lista_nueva = [i + 1e3 for i in list(libro_0['bid_size'])]
+
+# Compresion diccionario {"llave 1": objeto, "llave 2": objeto ...}
+llave_nueva = {"llave_"+str(i) : list(libro_0['bid_size'])[i] for i in range(0, len(list(libro_0['bid_size'])))}
+
+
+#plot_1 = vz.plot_lines(data_x=list(data_ob.keys()),
+#                       data_s1=)
