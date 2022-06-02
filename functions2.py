9a0929752967c18e959910d790d6c9786152818e
Moises Flores <floresmoisesortiz@gmail.com>
2022-06-02 16:29:57 -0500

    OOP implemented for orderbook measures

---
+++ functions2.py
@@ -0,0 +1,105 @@
+
+"""
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
+# -- script: functions.py : python script with general functions                                         -- #
+# -- author: YOUR GITHUB USER NAME                                                                       -- #
+# -- license: THE LICENSE TYPE AS STATED IN THE REPOSITORY                                               -- #
+# -- repository: YOUR REPOSITORY URL                                                                     -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+"""
+import numpy as np
+import pandas as pd
+
+
+class OrderBookMeasures:
+
+    """
+    Parameteters
+    ------------
+    data_ob: dict (default: None)
+        Datos de entrada del libro de ordenes, es un diccionario con la siguiente estructura:
+        'timestamp': objeto tipo timestamp reconocible por maquina, e.g. pd.to_datetime()
+        'bid_size': volume de niveles bid
+        'bid': precio de niveles bid
+        'ask': precio de niveles ask
+    """
+
+    def __init__(self, data_ob: dict) -> dict:
+        self.data_ob = data_ob
+
+    # Hidden Attributes
+    @property
+    def __ob_ts(self) -> list:
+        return list(self.data_ob.keys())
+
+    @property
+    def __l_ts(self) -> list:
+        return [pd.to_datetime(i_ts) for i_ts in self.__ob_ts]
+
+    # -- Median Time of OrderBook update -- #
+    def meadianTimeOB(self) -> float:
+        ob_m1 = np.median([self.__l_ts[n_ts + 1] - self.__l_ts[n_ts] for n_ts in range(0, len(self.__l_ts)-1)]).total_seconds() * 1000
+        return ob_m1
+
+    # -- Spread -- #
+    def spread(self) -> list:
+        ob_m2 = [self.data_ob[self.__ob_ts[0]]['ask'][0] 
+                 - self.data_ob[self.__ob_ts[0]]['bid'][0] 
+                for i in range(0, len(self.__ob_ts))]
+        return ob_m2
+
+    # -- Midprice -- #
+    def midPrice(self) -> list:
+        ob_m3 = [(self.data_ob[self.__ob_ts[0]]['ask'][0] 
+                + self.data_ob[self.__ob_ts[0]]['bid'][0])*0.5 
+                for i in range(0, len(self.__ob_ts))]
+        return ob_m3
+
+    # -- No. Price Levels -- #
+    def priceLevels(self) -> list:
+        ob_m4 = [self.data_ob[i_ts].shape[0] for i_ts in self.__ob_ts]
+        return ob_m4
+
+     # -- Bid_Volume -- #
+    def bidVolume(self) -> list:
+        ob_m5 = [np.round(self.data_ob[i_ts]['bid_size'].sum(), 6) 
+                 for i_ts in self.__ob_ts]
+        return ob_m5
+
+     # -- Ask_Volume -- #
+    def askVolume(self) -> list:
+        ob_m6 = [np.round(self.data_ob[i_ts]['ask_size'].sum(), 6) 
+                for i_ts in self.__ob_ts]
+        return ob_m6
+
+     # -- Total_Volume -- #
+    def totalVolume(self) -> list:
+        ob_m7 = [np.round(self.data_ob[i_ts]['bid_size'].sum() 
+                 + self.data_ob[i_ts]['ask_size'].sum(), 6) for i_ts in self.__ob_ts]
+        return ob_m7
+
+    # -- OrderBook Imbalance (v: volume, d: depth) -- #
+    # v[0] Bid volume, v[1] Ask volume 
+    def obImbalance(self) -> list:
+        obimb = lambda v, d: np.sum(v.iloc[:d,0])/np.sum([v.iloc[:d,0], v.iloc[:d,1]])
+        ob_m8 = [obimb(self.data_ob[i_ts][['bid_size','ask_size']],
+                 len(self.data_ob[i_ts])) for i_ts in self.__ob_ts]
+        return ob_m8
+
+    # -- wighted-Midprice (p: price, v: volume) -- #
+    def w_midPrice(self) -> list:
+        # w_midprice = lambda p, v: (v.iloc[:,1]/np.sum([v.iloc[:,0], v.iloc[:,1]]))*p.iloc[:,0] + (v.iloc[:,0]/np.sum([v.iloc[:,0], v.iloc[:,1]]))*p.iloc[:,1]
+        # ob_m9  = [w_midprice(data_ob[i_ts][['bid','ask']], data_ob[i_ts][['bid_size', 'ask_size']]) for i_ts in ob_ts]
+        ob_m8 = self.obImbalance()
+        ob_m3 = self.midPrice()
+        ob_m9 = [ob_m8[i_ts] * ob_m3[i_ts] for i_ts in range(0, len(self.__ob_ts))]
+        return ob_m9
+
+    # -- VWAP (Volume-Weighted Average Price) (p: price, v: volume, d:depth) -- #
+    def vwap(self) -> list:
+        # p[0]:Bid price, p[1]:Ask price, v[0]: Bid volume, v[1]: Ask volume
+        vwap = lambda p, v, d: np.sum(p.iloc[:d, 0] * v.iloc[:d,0] + p.iloc[:d,1] * v.iloc[:d,1]) / np.sum(v.iloc[:d,0] + v.iloc[:d,1])
+        ob_m10 = [vwap(self.data_ob[i_ts][['bid', 'ask']], self.data_ob[i_ts][['bid_size', 'ask_size']], 
+                 len(self.data_ob[i_ts])) for i_ts in self.__ob_ts]
+        return ob_m10
--- main.py
+++ main.py
@@ -8,11 +8,11 @@
 # -- repository: YOUR REPOSITORY URL                                                                     -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
-
 import data as dt
 import functions as fn
 import numpy as np
 import visualizations as vz
+from functions2 import OrderBookMeasures
 
 # Read input data
 data_ob = dt.ob_data
@@ -22,13 +22,14 @@ n_libros = len(list(data_ob.keys()))
 print(f"La cantidad total de libros de ordenes es: {n_libros}")
 
 # OrderBook Metrics
-data_1 = fn.f_descriptive_ob(data_ob=data_ob) 
+data_1 = fn.f_descriptive_ob(data_ob=data_ob)
+data_2 = OrderBookMeasures(data_ob=data_ob) 
 
 # Celda 3
-print(f"Mediana del tiempo esperado para un nuevo OrderBook: {data_1['median_ts_ob']}")
+# print(f"Mediana del tiempo esperado para un nuevo OrderBook: {data_1['median_ts_ob']}")
 
 # Celda 4
-print(f"Mediana del tiempo esperado para un nuevo OrderBook: {data_1['Orderbook Imbalance']}")
+print(f"Mediana del tiempo esperado para un nuevo OrderBook: {data_2.meadianTimeOB()}")
 
 # -- Ejercicios de repaso
 #data_ob[list(data_ob.keys())[1]]['bid_size'][0]
@@ -44,8 +45,8 @@ print(f"Mediana del tiempo esperado para un nuevo OrderBook: {data_1['Orderbook 
 #llave_nueva = {"llave_"+str(i) : list(libro_0['bid_size'])[i] for i in range(0, len(list(libro_0['bid_size'])))}
 
 # --Plot 1-- #
-plot_1 = vz.plot_lines(data_x=list(data_ob.keys()),
-                       data_s1=data_1['Orderbook Imbalance'],
-                       data_s2=data_1['wighted-Midprice'],
-                       data_s3=data_1['VWAP'])
-plot_1.show()
+# plot_1 = vz.plot_lines(data_x=list(data_ob.keys()),
+#                        data_s1=data_1['Orderbook Imbalance'],
+#                       data_s2=data_1['wighted-Midprice'],
+#                       data_s3=data_1['VWAP'])
+#plot_1.show()
